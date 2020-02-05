5872e48aac1c7da3a28e33706aa03d66138128b6
Youssef <youssefelhammoumi94@gmail.com>
2020-02-05 23:35:39 +0100

    Designing the blog data schema

---
+++ appweb/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from first_app.models import 
+
+admin.site.register(accessrecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(login)
