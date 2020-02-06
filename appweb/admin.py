b057d0795be59cdf3f518177b4a95c2d5ce2b220
Youssef <youssefelhammoumi94@gmail.com>
2020-02-06 01:28:50 +0100

    Building list and detail

--- appweb/admin.py
+++ appweb/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from first_app.models import 
+from .models import Post
 
-admin.site.register(accessrecord)
-admin.site.register(Topic)
-admin.site.register(Webpage)
-admin.site.register(login)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish','status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+admin.site.register(Post, PostAdmin)
--- appweb/views.py
+++ appweb/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
-from django.shortcuts import render
-from django.http import HttpResponse
-from first_app.models import login
-from django.bootstrap import bootstrap
-def index(request):
-    return (request,'first_app/Home.html')
-def login(request):
-    user=login.objects.order_by('first_name')
-    user2={"utilisateur":user}
-    return render(request,'first_app/login.html',context=user2)
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,'blog/post/list.html',{'posts': posts})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,status='published',publish__year=year,
+                             publish__month=month,publish__day=day)
+    return render(request,'blog/post/detail.html',{'post': post})
